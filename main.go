4b39c47d3afc5a0b31a4740d94382821156c6a7b
Đức Lã Minh <laminhduc2k1@gmail.com>
2024-05-14 19:09:18 +0700

    new release

--- main.go
+++ main.go
@@ -15,7 +15,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Chao mung ban den voi CMC TS"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
